d8d59aee66b9844b1fa4098a76e1035c8800318b
Dashulya-coder <dasha.ukshe@gmail.com>
2026-05-20 00:02:13 +0300

    feat: add AES encryption service

---
+++ src/main/java/CryptoService.java
@@ -0,0 +1,23 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CryptoService {
+
+    private final SecretKeySpec key;
+
+    public CryptoService(byte[] keyBytes) {
+        this.key = new SecretKeySpec(keyBytes, "AES");
+    }
+
+    public byte[] encrypt(byte[] data) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public byte[] decrypt(byte[] data) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+}
